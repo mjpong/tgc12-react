e92110ba08ab728d502dcafab75cb60f16b64fe5
mjpong <jolenepong@gmail.com>
2021-06-08 09:21:34 +0000

    function render and mapping done

--- form-handling/src/SurveyForm.js
+++ form-handling/src/SurveyForm.js
@@ -5,8 +5,70 @@ class SurveyForm extends React.Component {
         'name': '',
         'color': 'red',
         'country': 'singapore',
-        'items': []
+        'items': [],
+        'newColor': 'red',
+        'newCountry': '',
+        'fruits':[]
     }
+
+    countries = [
+        {
+            'display': 'Singapore',
+            'value': 'singapore'
+        },
+        {
+            'display': 'Malaysia',
+            'value': 'malaysia'
+        }, {
+            'display': 'Indonesia',
+            'value': 'indonesia'
+        }
+    ]
+    fruits = [{
+        'display': 'Apple',
+        'value': 'apple'
+    },
+    {
+        'display': 'Banana',
+        'value': 'banana'
+    },
+    {
+        'display': 'Cherries',
+        'value': 'cherries'
+    }]
+
+    colors = [{
+        'display': 'Red',
+        'value': 'red'
+    },
+    {
+        'display': 'Green',
+        'value': 'green'
+    }, {
+        'display': 'Blue',
+        'value': 'blue',
+
+    }
+    ]
+
+    renderColors() {
+        let options = [];
+        for (let color of this.colors) {
+            let e = (
+                <React.Fragment key={color.value}>
+                    <input name="newColor" type="radio" value={color.value}
+                        checked={this.state.newColor === color.value}
+                        onChange={this.updateFormField} />
+                    <span>{color.display}</span>
+                </React.Fragment>
+            )
+            options.push(e)
+        }
+        return options;
+    }
+
+
+
 
     updateFormField = (e) => {
         this.setState({
@@ -47,7 +109,7 @@ class SurveyForm extends React.Component {
 
     render() {
         return (
-            <React.Fragment>
+            <React.Fragment >
                 <div>
                     <label>Name: </label>
                     <input name="name" type="text" value={this.state.name} onChange={this.updateFormField} />
@@ -76,25 +138,53 @@ class SurveyForm extends React.Component {
                     <label>Items: </label>
                     <input type="checkbox"
                         name="items" value="passport"
-                        checked={this.state.items.includes('passport')} 
+                        checked={this.state.items.includes('passport')}
                         onChange={this.updateCheckBox} />Passport
                     <input type="checkbox"
                         name="items" value="visa"
-                        checked={this.state.items.includes('visa')} 
+                        checked={this.state.items.includes('visa')}
                         onChange={this.updateCheckBox} />Visa
                     <input type="checkbox"
                         name="items" value="luggage"
-                        checked={this.state.items.includes('luggage')} 
-                        onChange={this.updateCheckBox}/>Luggage
+                        checked={this.state.items.includes('luggage')}
+                        onChange={this.updateCheckBox} />Luggage
                     <input type="checkbox"
                         name="items" value="money"
                         checked={this.state.items.includes('money')}
                         onChange={this.updateCheckBox} />Money
                 </div>
+                <div>
+                    <label>Function Render Favorite Color: </label>
+                    {this.renderColors()}
+                </div>
+                <div>
+                    <label>Mapping Country: </label>
+                    <select name="newCountry" value={this.state.newCountry} onChange={this.updateFormField}>
+                        {this.countries.map((c) => <option key={c.value}
+                            value={c.value}>{c.display}</option>)}
+                    </select>
+                </div>
+                <div>
+                    <label>Mapping Fruits: </label>
+                    {this.fruits.map((f)=>(
+                        <React.Fragment>
+                            <input type='checkbox'
+                                key={f.value}
+                                name="fruits"
+                                value={f.value}
+                                checked={this.state.fruits.includes(f.value)}
+                                onChange={this.updateCheckBox}/>{f.display}
+                        </React.Fragment>
+                    ))}
+
+                </div>
+
                 <button onClick={this.showFormValue}>Submit</button>
             </React.Fragment >
         )
     }
+
+
 }
 
 export default SurveyForm
